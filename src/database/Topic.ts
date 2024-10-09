ed31cf08720afcd276eb62a511786c64df6b8d19
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-09 08:00:13 +0200

    feat(schemas): add missing properties for Topic

--- src/database/Topic.ts
+++ src/database/Topic.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
 
 const schema = new mongoose.Schema({
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  order: { type: Number, required: true },
   done: { type: Boolean, default: false },
-  title: { type: String, required: true },
-  description: { type: String, required: true }
+  title: { type: String, required: false, default: "" },
+  description: { type: String, required: false, default: "" },
+  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Topic', required: false }
 });
 
 export type Topic = mongoose.InferSchemaType<typeof schema>;
