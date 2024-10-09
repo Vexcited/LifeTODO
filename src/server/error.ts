937eaa99b4deb360f6bab248d46361082ed03144
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-09 08:02:20 +0200

    feat(server): add handle error utility

--- src/server/error.ts
+++ src/server/error.ts
@@ -1,1 +1,9 @@
 export const error = (message: string, status: number) => new Response(message, { status });
+export const handleError = (e: any) => {
+  if (e instanceof Response) {
+    return e;
+  }
+
+  console.error(e);
+  return error("internal server error.", 500);
+};
